461bb5ba20de88cd7ac70428a60206a95c636638
zhouhaoxi <930469901@qq.com>
2016-08-03 16:31:46 +0800

    init

---
+++ TestMaven/src/main/java/com/jzm/TestMaven/zhouhaoxi.java
@@ -0,0 +1,25 @@
+
+package com.jzm.TestMaven;
+
+import java.io.Serializable;
+
+/**
+ * @author 周浩西
+ * @date   创建时间：2016年8月3日 下午4:30:32 
+ * @version 1.0 
+ * @description 
+ */
+public class zhouhaoxi implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	public static void main(String[] args) {
+		System.out.println("我的名字叫周浩西");
+	}
+	
+	
+}
